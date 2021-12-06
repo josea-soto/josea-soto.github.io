8a6fbe0be6ce7113c4a50090c7852dcc53ff11ce
Jose Alberto Soto <asotorev@gmail.com>
2021-12-05 22:14:35 -0600

    updated js function to display weather forecast

--- lesson10/js/weatherApi.js
+++ lesson10/js/weatherApi.js
@@ -2,6 +2,9 @@ const weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&ap
 const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=92e8868f277c47300b3b3258e89d9053&lang=en&units=imperial"
 let temperature = 0
 let windspeed  = 0
+let newDate = '';
+let weekForecast = {}
+  
 
 function windchill(){    
     let windchill = ''
@@ -10,7 +13,7 @@ function windchill(){
         windchill = "N/A"
     }
     else {
-        windchill = ((35.74) + (0.6215 * temperature) - (35.75 * (windspeed ** 0.16)) + (0.4275 * temperature * (windspeed ** 0.16))).toFixed(2)
+        windchill = Math.round((35.74) + (0.6215 * temperature) - (35.75 * (windspeed ** 0.16)) + (0.4275 * temperature * (windspeed ** 0.16))).toFixed(2)
     }
     return windchill
 }
@@ -28,17 +31,32 @@ fetch(weatherURL)
     .then(() => {
         temperature = parseFloat(document.getElementById("temperature").innerText)
         windspeed = parseFloat(document.getElementById("windspeed").innerText)
-        document.getElementById("windchill").innerText = Math.round(windchill(temperature, windspeed));
+        
+        document.getElementById("windchill").innerText = windchill(temperature, windspeed);
     });
 
 fetch(forecastURL)
     .then((response) => response.json())
     .then((data) => {
-        console.log('forecast URL');
 
-        let forecast = [];
-        
-        forecast = data.list.filter(el => el.dt_txt.indexOf('18:00:00') >= 0)
-
-        console.log('forecast',forecast)
+        newDate = '';
+        weekForecast = {}
+          
+        weekForecast = Object.assign({}, ...data.list
+            .filter(el => el.dt_txt.indexOf('18:00:00') >= 0)
+            .map((el, index) => {
+                    newDate = new Date(el.dt_txt)
+                    return({
+                       [days[newDate.getDay()] + " " + newDate.getDate()] : Math.round(el.main.temp) 
+                    })
+                }       
+            ));
+    })
+    .then(() =>{
+        let keys =  Object.keys(weekForecast)
+        let values =  Object.values(weekForecast)
+        for(i=0; i< keys.length; i++){
+            document.getElementById("dayName" + (i + 1)).innerText = keys[i];
+            document.getElementById("dayTemp" + (i + 1)).innerText = values[i] + " °C";
+        }
     });
