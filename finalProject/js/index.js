448062c4f096277ca392e8d1c77833dfbab2bf59
Jose Alberto Soto <asotorev@gmail.com>
2021-12-16 14:53:54 -0600

    added header, nav and home page

---
+++ finalProject/js/index.js
@@ -0,0 +1,26 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+setTimeout(() => {
+    let date = new Date();
+    let currentYear = date.getFullYear()
+
+    document.querySelector("#current-year").textContent +=  currentYear 
+    document.querySelector("#last-updated").textContent = days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear(); 
+}, 10)
+
+
+function toggleMenu() {        
+    document.getElementById("nav-list").classList.toggle("show-menu");
+    
+    let menuImage = document.getElementById("menu-logo")
+
+    if (menuImage.src.indexOf ("x.svg") >= 0) {
+        menuImage.src = "./images/menu.svg";
+    }
+    else 
+    {
+        menuImage.src = "./images/x.svg";
+    }
+}
+
